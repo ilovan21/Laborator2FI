5f718584a5b7cf0cb83eb912c3605bf5595b1473
ilovm2 <99871534+ilovm2@users.noreply.github.com>
2022-10-22 00:50:29 +0300

    Update Flori.java

--- src/incapsulare/Flori.java
+++ src/incapsulare/Flori.java
@@ -9,27 +9,33 @@ public class Flori {
     public void setFamilia(String familia){
         this.familia = familia;
     }
+    
     public String getFamilia(){
         return this.familia;
     }
+    
     public void setCuloare(String culoare){
         this.culoare=culoare;
     }
+    
     public String getCuloare(){
         return this.culoare;
     }
-
+    
     public void setNrPetale(int nrPetale){
         this.nrPetale=nrPetale;
     }
+    
     public int getNrPetale(){
         return this.nrPetale;
     }
+    
     Flori(String clasa, int nrPetale, String culoare){
         this.familia =clasa;
         this.nrPetale=nrPetale;
         this.culoare=culoare;
     }
+    
     public String toString(){
         return " Familia: "+familia +" Nr de petale: "+nrPetale+" Culoarea: "+culoare;
     }
