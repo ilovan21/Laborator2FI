ab51cc2aca46e52bc78b24a2cf128c93ceb919d9
ilovm2 <99871534+ilovm2@users.noreply.github.com>
2022-10-22 00:49:52 +0300

    Update Copaci.java

--- src/incapsulare/Copaci.java
+++ src/incapsulare/Copaci.java
@@ -12,21 +12,27 @@ public class Copaci {
         this.roditor=roditor;
         this.varsta=varsta;
     }
+    
     public void setClasa(String clasa){
         this.clasa=clasa;
     }
+    
     public String getClasa(){
         return this.clasa;
     }
+    
     public void setMarime(Double marime){
         this.marime=marime;
     }
+    
     public Double getMarime(){
         return this.marime;
     }
+    
     public void isRoditor(boolean roditor){
         this.roditor=roditor;
     }
+    
     public boolean getRoditor(){
         return this.roditor;
     }
